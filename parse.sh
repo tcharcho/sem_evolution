ff2aa6df29aa668ef244006d6c10c4bc9e85577a
tamara <tamara@focus21.io>
2019-03-10 18:47:55 -0400

    Dynamic parse script

--- parse.sh
+++ parse.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-for file in full_run_1/*.sem
-do
-  # echo $file
-  filename="$(cut -d'/' -f2 <<<"$file")"
-  # echo $filename
-  newFile="$(cut -d'.' -f1 <<<"$filename")"
-  # echo $newFile
 
-  grep fit $file | sort -n > full_run_1/$newFile.dat
-done
+if [ "$1" != "" ]; then
+  for file in $1/*.sem
+  do
+    # echo $file
+    filename="$(cut -d'/' -f2 <<<"$file")"
+    # echo $filename
+    newFile="$(cut -d'.' -f1 <<<"$filename")"
+    # echo $newFile
+
+    grep fit $file | sort -n > $1/$newFile.dat
+  done
+else
+  echo "Must provide name for directory"
+fi
+
 
 
 

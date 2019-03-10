292a07be2561ac88ffa43096dc67c9a4e6991a40
tamara <tamara@focus21.io>
2019-03-10 19:53:13 -0400

    Group data based on hyperparameter values

--- parse.sh
+++ parse.sh
@@ -12,8 +12,32 @@ if [ "$1" != "" ]; then
     grep fit $file | sort -n > $1/$newFile.dat
 
     sed -i '' 's/ -fitness//g' $1/$newFile.dat
-
   done
+
+  for pop in 10 100 1000
+  do
+    for file in $1/*_${pop}_*.dat
+    do
+      cat $file >> "pop_$pop"
+    done
+  done
+
+  for mnm in 3 7 9
+  do
+    for file in $1/*_${mnm}_*.dat
+    do
+      cat $file >> "mnm_$mnm"
+    done
+  done
+
+  for states in 6 18 24
+  do
+    for file in $1/*_${states}.dat
+    do
+      cat $file >> "states_$states"
+    done
+  done
+
 else
   echo "Must provide name for directory"
 fi

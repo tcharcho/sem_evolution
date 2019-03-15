f3c047a08f645eb0a51900109e546eabdb3312fc
tamara <tamara@focus21.io>
2019-03-15 13:02:50 -0400

    Comments with old hyperparameters

--- test_run.sh
+++ test_run.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # popsize
-for i in 5 2000 5000
+for i in 5 2000 5000 #10 100 1000
 do
   # MNM
   for j in 3 7 9
   do
     # states
-    for k in 3 50 70
+    for k in 3 50 70 #6 18 24
     do
       g++ -lm -O3 SemDistEvo.cpp sem.cpp stat.cpp -Dpopsize=$i -DMNM=$j -DSTATES=$k
       ./a.out $1
